0aca44d63a79e5870ce65d4268fb1850b217b258
GeneralSolutionsSoftware <GeneralSolutionsSoftware@gmail.com>
2016-01-02 15:50:40 -0600

    Add missing files
    Missing files are added back

---
+++ GeneralSolutions.Core/CSVReaderModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace GeneralSolutions
+{
+    public class CSVReaderModule<InputType> : ModuleBase<TextReader>, IDataReader<List<InputType>>
+    {
+        static IEnumerable<PropertyInfo> properties = typeof(InputType)
+            .GetProperties()
+            .Where( p => p.CanRead && p.MemberType == MemberTypes.Property && 
+                    p.PropertyType.Namespace == "System" && !p.PropertyType.IsArray);
+
+        public List<InputType> Read()
+        {
+            String[] header = ReadHeader();
+            
+            List<InputType> list = new List<InputType>();
+
+            String line = Context.ReadLine();
+            while(line != null)
+            {
+                InputType item = ReadRow(line);
+                list.Add(item);
+            }
+                        
+            return list;            
+        }
+
+        private InputType ReadRow(string line)
+        {
+            InputType item = Activator.CreateInstance<InputType>();
+
+            return item;
+        }
+
+        private string[] ReadHeader()
+        {
+            String[] header = null;
+
+            String line = Context.ReadLine();
+            if (line != null)
+                header = line.Split(',');
+
+            return header;
+        }
+
+
+    }
+}
---
+++ GeneralSolutions/ItemDTO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralSolutions
+{
+    [Serializable]
+    public class ItemDTO
+    {
+        public ItemDTO()
+        {
+        }
+
+        public ItemDTO(Item i)
+            : this()
+        {
+            Id = i.Id;
+            Number = i.Number;
+            Color = i.Color;
+            Weight = i.Weight;
+            IsAvailable = i.IsAvailable;
+            PurchaseDate = i.PurchaseDate;
+            Guid = i.Guid;
+            CategoryId = i.CategoryId;
+            FileName = i.FileName;
+            Price = i.Price;
+            Model = i.Model;
+        }
+
+        public int Id { get; set; }
+        public long Number { get; set; }
+        public string Color { get; set; }
+        public Nullable<decimal> Weight { get; set; }
+        public Nullable<bool> IsAvailable { get; set; }
+        public Nullable<System.DateTime> PurchaseDate { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<System.Guid> Guid { get; set; }
+        public string FileName { get; set; }
+        public Nullable<int> CategoryId { get; set; }
+        public string Model { get; set; }
+
+        public Item ToItem()
+        {
+            return new Item
+            {
+                Id = this.Id,
+                Number = this.Number,
+                Color = this.Color,
+                Weight = this.Weight,
+                IsAvailable = this.IsAvailable,
+                PurchaseDate = this.PurchaseDate,
+                Guid = this.Guid,
+                CategoryId = this.CategoryId,
+                FileName = this.FileName,
+                Price = this.Price,
+                Model = this.Model
+
+            };
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// ItemDTO is required because XML serialization does not suppport virtual properties of Item class.
+// Implementation: Parameterless Constructor is required for XML serialization
